5877af71e638114a36efc8f9e5026c377990712a
DESKTOP-CQH3EL9\parkm <parkmyounghoi@gmail.com>
2021-09-14 20:14:11 +0900

    20210914

---
+++ src/main/java/com/daeso/nar01/Nar01Application.java
@@ -0,0 +1,13 @@
+package com.daeso.nar01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nar01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nar01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/daeso/nar01/Nar01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daeso.nar01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nar01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
